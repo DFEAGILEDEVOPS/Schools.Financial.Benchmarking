53440291eddb2f9eeec06e2761f042e12afce355
Serkan Sozer <eserkansozer@hotmail.com>
2018-05-21 16:19:42 +0100

    #21056-view more tables

--- Web/SFB.Web.UI/Assets/Scripts/app.js
+++ Web/SFB.Web.UI/Assets/Scripts/app.js
@@ -111,12 +111,14 @@
             var $showChartsButton = $('a.view-charts-tables.charts');
             var $showTablesButton = $('a.view-charts-tables.tables');
             var $saveAsImagesButtons = $('.save-as-image');
+            var $viewMoreLabels = $("a span.view-more");
             if (mode === 'Charts') {
                 $showChartsButton.hide();
                 $showTablesButton.show();
                 $tables.hide();
                 $charts.show();
                 $saveAsImagesButtons.show();
+                $viewMoreLabels.text("View more charts");
                 sessionStorage.chartFormat = 'Charts';
             } else if (mode === 'Tables') {
                 $showTablesButton.hide();
@@ -124,6 +126,7 @@
                 $charts.hide();
                 $tables.show();
                 $saveAsImagesButtons.hide();
+                $viewMoreLabels.text("View more tables");
                 sessionStorage.chartFormat = 'Tables';
             }
         }
