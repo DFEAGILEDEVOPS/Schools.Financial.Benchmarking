08c97061878dd59cac463790b44c49d9c80ab439
Serkan Sozer <eserkansozer@hotmail.com>
2018-07-09 12:03:14 +0100

    #22249

--- Web/SFB.Web.UI/Assets/Scripts/app.js
+++ Web/SFB.Web.UI/Assets/Scripts/app.js
@@ -393,4 +393,20 @@
     Math.log10 = Math.log10 || function (x) {
         return Math.log(x) * Math.LOG10E;
     };
+
+    //polyfill for includes()
+    if (!String.prototype.includes) {
+        String.prototype.includes = function (search, start) {
+            'use strict';
+            if (typeof start !== 'number') {
+                start = 0;
+            }
+
+            if (start + search.length > this.length) {
+                return false;
+            } else {
+                return this.indexOf(search, start) !== -1;
+            }
+        };
+    }
 }());
