2e97c2cf634cbf4747a24ad48d0c92ed4a8d7220
Serkan Sozer <eserkansozer@hotmail.com>
2018-12-11 11:43:40 +0000

    #27267

---
+++ Web/SFB.Web.UI/Controllers/PocController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SFB.Web.UI.Controllers
+{
+    public class PocController : Controller
+    {
+        // GET: Poc
+        public ActionResult LocationSearch()
+        {
+            return View();
+        }
+    }
+}
