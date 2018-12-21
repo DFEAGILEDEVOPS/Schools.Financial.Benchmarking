690e188645aee24edac94af13ee928d7d3a848e4
Serkan Sozer <eserkansozer@hotmail.com>
2018-12-21 10:51:41 +0000

    #27636

--- Web/SFB.Web.UI/Assets/Scripts/app.js
+++ Web/SFB.Web.UI/Assets/Scripts/app.js
@@ -413,4 +413,13 @@
             }
         };
     }
+
+    //polyfill for startsWith()
+    if (!String.prototype.startsWith) {
+        Object.defineProperty(String.prototype, 'startsWith', {
+            value: function (search, pos) {
+                return this.substr(!pos || pos < 0 ? 0 : +pos, search.length) === search;
+            }
+        });
+    }
 }());
