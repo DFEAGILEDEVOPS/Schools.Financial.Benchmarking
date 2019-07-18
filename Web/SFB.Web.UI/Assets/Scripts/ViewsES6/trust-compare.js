3dec42d60ead27c97e40a39389bab3f01df2c6a7
Serkan Sozer <eserkansozer@hotmail.com>
2019-07-18 15:40:11 +0100

    WCAG-Trust comparison

--- Web/SFB.Web.UI/Assets/Scripts/ViewsES6/trust-compare.js
+++ Web/SFB.Web.UI/Assets/Scripts/ViewsES6/trust-compare.js
@@ -195,13 +195,13 @@
                 setTimeout(function () { $("#schoolCount").text(count + " trusts found"); }, 500);
                 $("button.view-benchmark-charts").attr("aria-label", "View " + count + " trusts in a benchmark chart");
                 $("#liveCountBar").show();
-                if (count > 0) {
-                    $("button.submit").show();
-                    $("button.submit").removeAttr("disabled");
-                } else {
-                    $("button.submit").hide();
-                    $("button.submit").attr("disabled", "disabled");
-                }
+                //if (count > 0) {
+                //    $("button.submit").show();
+                //    $("button.submit").removeAttr("disabled");
+                //} else {
+                //    $("button.submit").hide();
+                //    $("button.submit").attr("disabled", "disabled");
+                //}
                 $('.sticky-div').Stickyfill();
             });
     }
