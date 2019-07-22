b96f4f38808bd23ea95751ad6e22d12438ee7742
Serkan Sozer <eserkansozer@hotmail.com>
2019-07-22 15:11:30 +0100

    #34363

--- Web/SFB.Web.UI/Assets/Scripts/ViewsES6/benchmark-charts.js
+++ Web/SFB.Web.UI/Assets/Scripts/ViewsES6/benchmark-charts.js
@@ -811,6 +811,7 @@
             <div class='form-group'><label class='form-label' for='saveUrl'>Page link</label>
                 <input id='saveUrl' name='saveUrl' type='text' class='form-control save-url-input' value='${link}'>
                 <button id='clip-button' class='button' type='button' data-clipboard-target='#saveUrl'>Copy link to clipboard</button>
+                <span class='error-message' style='display: none'>Please select and copy the link above.</span>
             </div>         
             <a class='bold-xsmall' href="mailto:?subject=Saved%20benchmark%20charts&body=Here%20is%20your%20saved%20benchmark%20basket:%20${link}">
             <img class="icon email-list-icon" src="/public/assets/images/icons/icon-email.png" alt="" />Email the link</a>            
@@ -841,6 +842,11 @@
         clipboard.on('success', function () {
             DfE.Views.BenchmarkChartsViewModel.ShowSaveModalTwo();
         });
+
+        if (!ClipboardJS.isSupported()) {
+            $('#clip-button').hide();
+            $('#clip-not-supported').show();
+        }
 
 
         $('#js-modal-close').focus();
