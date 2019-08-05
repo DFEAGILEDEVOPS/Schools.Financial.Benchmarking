408df3f500167fa236aeedad619840a79109e312
Serkan Sozer <eserkansozer@hotmail.com>
2019-08-05 14:59:58 +0100

    #33931-UI

--- Web/SFB.Web.UI/Controllers/HelpController.cs
+++ Web/SFB.Web.UI/Controllers/HelpController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using SFB.Web.UI.Models;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
 
 namespace SFB.Web.UI.Controllers
 {
@@ -17,6 +20,26 @@ namespace SFB.Web.UI.Controllers
         public ActionResult DataSources()
         {
             return View();
+        }
+
+        public ActionResult DataQueries()
+        {
+            ViewBag.ModelState = ModelState;
+            return View(new DataQueryViewModel());
+        }
+
+        public ActionResult DataQuerySubmission(DataQueryViewModel dataQuery)
+        {
+            ViewBag.ModelState = ModelState;
+            if (ModelState.IsValid)
+            {
+                //Todo: Generate reference number and send emails.
+                return View("DataQueryConfirmation");
+            }
+            else
+            {                
+                return View("DataQueries", dataQuery);
+            }
         }
     }
 }
---
+++ Web/SFB.Web.UI/Models/DataQueryViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SFB.Web.UI.Models
+{
+    public class DataQueryViewModel
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public string Reference { get; set; }
+
+        [Required(ErrorMessage = "Data query field is required.")]
+        [StringLength(1000, MinimumLength = 2, ErrorMessage = "The field Data query must be a string with a minimum length of 2 and a maximum length of 1000.")]
+        public string DataQuery { get; set; }        
+    }
+}
