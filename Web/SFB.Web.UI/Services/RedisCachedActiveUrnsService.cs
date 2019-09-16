2bbb247caaa3b3bad24b1b8965a0a702a744fe0d
Serkan Sozer <eserkansozer@hotmail.com>
2019-09-16 13:58:14 +0100

    Revert "Redis - REVERT"
    This reverts commit 18479328b376c10b0d08fd1cb5fcbe73771b0509.

--- Web/SFB.Web.UI/Services/RedisCachedActiveUrnsService.cs
+++ Web/SFB.Web.UI/Services/RedisCachedActiveUrnsService.cs
@@ -17,57 +17,55 @@ namespace SFB.Web.UI.Services
         /// <summary>
         /// //Clear the active urns cache at each load of the web application(in prod builds). 
         /// </summary>
-        //public RedisCachedActiveUrnsService(IContextDataService contextDataService)
-        //{
-        //    _contextDataService = contextDataService;
+        public RedisCachedActiveUrnsService(IContextDataService contextDataService)
+        {
+            _contextDataService = contextDataService;
 
-        //    #if !DEBUG
-        //       ClearCachedData();
-        //    #endif
-        //}
+            #if !DEBUG
+               ClearCachedData();
+            #endif
+        }
 
-        //private static Lazy<ConnectionMultiplexer> lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
-        //{
-        //    string cacheConnection = ConfigurationManager.AppSettings["RedisConnectionString"].ToString();
-        //    return ConnectionMultiplexer.Connect(cacheConnection);
-        //});
+        private static Lazy<ConnectionMultiplexer> lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+        {
+            string cacheConnection = ConfigurationManager.AppSettings["RedisConnectionString"].ToString();
+            return ConnectionMultiplexer.Connect(cacheConnection);
+        });
 
-        //public static ConnectionMultiplexer Connection
-        //{
-        //    get
-        //    {
-        //        return lazyConnection.Value;
-        //    }
-        //}
+        public static ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return lazyConnection.Value;
+            }
+        }
 
         public List<int> GetAllActiveUrns()
         {
-            return null;
-
-            //var cache = Connection.GetDatabase();
+            var cache = Connection.GetDatabase();
             
-            //var serializedList = cache.StringGet("SFBActiveURNList");
+            var serializedList = cache.StringGet("SFBActiveURNList");
 
-            //List<int> deserializedList;
+            List<int> deserializedList;
 
-            //if (serializedList.IsNull)
-            //{
-            //    deserializedList = _contextDataService.GetAllSchoolUrns();
+            if (serializedList.IsNull)
+            {
+                deserializedList = _contextDataService.GetAllSchoolUrns();
 
-            //    cache.StringSet("SFBActiveURNList", JsonConvert.SerializeObject(deserializedList));
-            //}
-            //else
-            //{
-            //    deserializedList = JsonConvert.DeserializeObject<List<int>>(serializedList);
-            //}
+                cache.StringSet("SFBActiveURNList", JsonConvert.SerializeObject(deserializedList));
+            }
+            else
+            {
+                deserializedList = JsonConvert.DeserializeObject<List<int>>(serializedList);
+            }
             
-            //return deserializedList;
+            return deserializedList;
         }
 
-        //private void ClearCachedData()
-        //{
-        //    var cache = Connection.GetDatabase();
-        //    cache.KeyDelete("SFBActiveURNList");
-        //}
+        private void ClearCachedData()
+        {
+            var cache = Connection.GetDatabase();
+            cache.KeyDelete("SFBActiveURNList");
+        }
     }
 }
