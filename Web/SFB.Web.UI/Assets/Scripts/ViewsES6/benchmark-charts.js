4e0fb6a43acc834cecd2e1a1e07cbbc05f43ab7a
Serkan Sozer <eserkansozer@hotmail.com>
2019-10-28 16:52:30 +0000

    #37607

--- Web/SFB.Web.UI/Assets/Scripts/ViewsES6/benchmark-charts.js
+++ Web/SFB.Web.UI/Assets/Scripts/ViewsES6/benchmark-charts.js
@@ -967,7 +967,7 @@
             </p>
             <div class='form-group'><label class='form-label' for='saveUrl'>Page link</label>
                 <input id='saveUrl' name='saveUrl' type='text' class='form-control save-url-input' value='${link}'>
-                <button id='clip-button' class='button' type='button' data-clipboard-target='#saveUrl'>Copy link to clipboard</button>
+                <button id='clip-button' class='button' type='button' data-clipboard-target='#saveUrl' style='font-size: 16px'>Copy link to clipboard</button>
                 <span id='clip-not-supported' class='error-message' style='display: none'>Please select and copy the link above.</span>
             </div>         
             <a class='bold-xsmall email-the-link' href="mailto:?subject=Saved%20benchmark%20charts&body=Here%20is%20your%20saved%20benchmark%20basket:%20${link}">
