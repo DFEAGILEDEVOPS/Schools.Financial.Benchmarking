a6b7850dbf04795932a921a20ed5fc6f019174e3
Serkan Sozer <eserkansozer@hotmail.com>
2020-03-12 14:26:59 +0000

    EM-fixing json serialization

--- Web/SFB.Web.ApplicationCore/Entities/EdubaseDataObject.cs
+++ Web/SFB.Web.ApplicationCore/Entities/EdubaseDataObject.cs
@@ -6,90 +6,90 @@ namespace SFB.Web.ApplicationCore.Entities
     public class EdubaseDataObject
     {
         [JsonProperty(PropertyName = EdubaseDataFieldNames.URN)]
-        public int URN;
+        public int URN { get; set; }
 
         [JsonProperty(PropertyName = EdubaseDataFieldNames.ESTAB_NAME)]
-        public string EstablishmentName;
+        public string EstablishmentName { get; set; }
 
         [JsonProperty(PropertyName = EdubaseDataFieldNames.OVERALL_PHASE)]
-        public string OverallPhase;
+        public string OverallPhase { get; set; }
 
         [JsonProperty(PropertyName = EdubaseDataFieldNames.PHASE_OF_EDUCATION)]
-        public string PhaseOfEducation;
+        public string PhaseOfEducation { get; set; }
 
         [JsonProperty(PropertyName = EdubaseDataFieldNames.TYPE_OF_ESTAB)]
-        public string TypeOfEstablishment;
+        public string TypeOfEstablishment { get; set; }
 
         [JsonProperty(PropertyName = EdubaseDataFieldNames.STREET)]
-        public string Street;
+        public string Street { get; set; }
 
         [JsonProperty(PropertyName = EdubaseDataFieldNames.TOWN)]
-        public string Town;
+        public string Town { get; set; }
 
         [JsonProperty(PropertyName = EdubaseDataFieldNames.LOCATION)]
-        public LocationDataObject Location;
+        public LocationDataObject Location { get; set; }
 
         [JsonProperty(PropertyName = EdubaseDataFieldNames.POSTCODE)]
-        public string Postcode;
+        public string Postcode { get; set; }
 
         [JsonProperty(PropertyName = EdubaseDataFieldNames.TRUSTS)]
-        public string Trusts;
+        public string Trusts { get; set; }
 
         [JsonProperty(PropertyName = EdubaseDataFieldNames.MAT_NUMBER)]
-        public string MATNumber;
+        public string MATNumber { get; set; }
 
         [JsonProperty(PropertyName = EdubaseDataFieldNames.COMPANY_NUMBER)]
-        public int? CompanyNumber;
+        public int? CompanyNumber { get; set; }
 
         [JsonProperty(PropertyName = EdubaseDataFieldNames.LA_CODE)]
-        public int LACode;
+        public int LACode { get; set; }
 
         [JsonProperty(PropertyName = EdubaseDataFieldNames.ESTAB_NO)]
-        public int EstablishmentNumber;
+        public int EstablishmentNumber { get; set; }
 
         [JsonProperty(PropertyName = EdubaseDataFieldNames.LA_ESTAB)]
-        public int LAEstab;
+        public int LAEstab { get; set; }
 
         [JsonProperty(PropertyName = EdubaseDataFieldNames.TEL_NO)]
-        public string TelephoneNum;
+        public string TelephoneNum { get; set; }
 
         [JsonProperty(PropertyName = EdubaseDataFieldNames.NO_PUPIL)]
-        public float? NumberOfPupils;
+        public float? NumberOfPupils { get; set; }
 
         [JsonProperty(PropertyName = EdubaseDataFieldNames.STAT_LOW)]
-        public int StatutoryLowAge;
+        public int StatutoryLowAge { get; set; }
 
         [JsonProperty(PropertyName = EdubaseDataFieldNames.STAT_HIGH)]
-        public int StatutoryHighAge;
+        public int StatutoryHighAge { get; set; }
 
         [JsonProperty(PropertyName = EdubaseDataFieldNames.HEAD_FIRST_NAME)]
-        public string HeadFirstName;
+        public string HeadFirstName { get; set; }
 
         [JsonProperty(PropertyName = EdubaseDataFieldNames.HAS_NURSERY)]
-        public string HasNursery;
+        public string HasNursery { get; set; }
 
         [JsonProperty(PropertyName = EdubaseDataFieldNames.HEAD_LAST_NAME)]
-        public string HeadLastName;
+        public string HeadLastName { get; set; }
 
         [JsonProperty(PropertyName = EdubaseDataFieldNames.OFFICIAL_6_FORM)]
-        public string OfficialSixthForm;
+        public string OfficialSixthForm { get; set; }
 
         [JsonProperty(PropertyName = EdubaseDataFieldNames.SCHOOL_WEB_SITE)]
-        public string SchoolWebsite;
+        public string SchoolWebsite { get; set; }
 
         [JsonProperty(PropertyName = EdubaseDataFieldNames.OFSTED_RATING)]
-        public string OfstedRating;
+        public string OfstedRating { get; set; }
 
         [JsonProperty(PropertyName = EdubaseDataFieldNames.OFSTE_LAST_INSP)]
-        public string OfstedLastInsp;
+        public string OfstedLastInsp { get; set; }
 
         [JsonProperty(PropertyName = EdubaseDataFieldNames.FINANCE_TYPE)]
-        public string FinanceType;
+        public string FinanceType { get; set; }
 
         [JsonProperty(PropertyName = EdubaseDataFieldNames.OPEN_DATE)]
-        public string OpenDate;
+        public string OpenDate { get; set; }
 
         [JsonProperty(PropertyName = EdubaseDataFieldNames.CLOSE_DATE)]
-        public string CloseDate;
+        public string CloseDate { get; set; }
     }
 }
--- Web/SFB.Web.ApplicationCore/Models/EfficiencyMetricModel.cs
+++ Web/SFB.Web.ApplicationCore/Models/EfficiencyMetricModel.cs
@@ -1,11 +1,15 @@
-﻿using SFB.Web.ApplicationCore.Entities;
+﻿using Newtonsoft.Json;
+using SFB.Web.ApplicationCore.Entities;
 
 namespace SFB.Web.ApplicationCore.Models
 {
     public class EfficiencyMetricModel
     {
+        [JsonProperty]
         public EfficiencyMetricDataObject EfficiencyMetricData { get; set; }
+        [JsonProperty]
         public EdubaseDataObject ContextData { get; set; }
+        [JsonProperty]
         public SchoolTrustFinancialDataObject FinancialData { get; set; }
     }
 }
