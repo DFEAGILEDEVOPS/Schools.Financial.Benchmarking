34a6c17a2e02d57a00aff6dba30971d1afe53ba1
Serkan Sozer <eserkansozer@hotmail.com>
2020-05-22 11:56:27 +0100

    Removing API project

--- Web/Web.UI.Automation/features/step_definitions/error_page_steps.rb
+++
@@ -1,7 +0,0 @@
-Given(/^I have navigated to a page that does not exist$/) do
-  visit Capybara.app_host + '/ssdkfslk'
-end
-
-Then(/^I should be shown an error page$/) do
-  expect(error_page).to be_all_there
-end
