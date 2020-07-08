69ed1064a915307d55b31170333e5cbe702cc33d
Serkan Sozer <eserkansozer@hotmail.com>
2020-07-08 17:36:31 +0100

    Cookie sharing

--- Web/SFB.Web.UI/public/govuk_template/assets/javascripts/govuk-template.js
+++ Web/SFB.Web.UI/public/govuk_template/assets/javascripts/govuk-template.js
@@ -28,6 +28,23 @@
     } else {
       return GOVUK.getCookie(name);
     }
+    };
+  GOVUK.setDomainCookie = function (name, value, options, domain) {
+        if (typeof options === 'undefined') {
+            options = {};
+        }
+        var cookieString = name + "=" + value + "; path=/";
+        if (options.days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (options.days * 24 * 60 * 60 * 1000));
+            cookieString = cookieString + "; expires=" + date.toGMTString();
+        }
+        if (document.location.protocol == 'https:') {
+            cookieString = cookieString + "; Secure";
+        }
+        cookieString = cookieString + "; samesite=lax";
+        cookieString = cookieString + "; domain=" + domain;
+        document.cookie = cookieString;
   };
   GOVUK.setCookie = function (name, value, options) {
     if(typeof options === 'undefined') {
