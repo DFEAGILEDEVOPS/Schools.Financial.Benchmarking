d73092f6c97905ef3e5ee38fcee143c2f6276f3e
Serkan Sozer <eserkansozer@hotmail.com>
2020-10-30 12:52:55 +0000

    AB#44144

--- Web/SFB.Web.UI/Controllers/NewsController.cs
+++ Web/SFB.Web.UI/Controllers/NewsController.cs
@@ -5,7 +5,9 @@ namespace SFB.Web.UI.Controllers
 {
     public class NewsController : Controller
     {
+        #if !DEBUG
         [OutputCache (Duration=28800, VaryByParam= "referrer", Location = OutputCacheLocation.Server, NoStore=true)]
+        #endif
         public ActionResult Index(string referrer)
         {
             ViewBag.referrer = referrer;
