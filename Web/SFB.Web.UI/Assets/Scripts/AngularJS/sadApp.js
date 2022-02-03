3e653d25f2a1b4454f163d7bc471324db7d2f77f
Serkan Sozer <serkan@groovytechs.co.uk>
2022-02-03 13:34:53 +0000

    AB#49774 - bandings

--- Web/SFB.Web.UI/Assets/Scripts/AngularJS/sadApp.js
+++ Web/SFB.Web.UI/Assets/Scripts/AngularJS/sadApp.js
@@ -7,33 +7,52 @@ var app = angular.module('sadApp', [])
                 $scope.sad = null;
                 var urn = DfE.Util.QueryString.get('urn');
                 var fuid = DfE.Util.QueryString.get('fuid');
-                var id = urn || fuid;
+                $scope.id = urn || fuid;
 
                 self.loadData = function () {
-                    $http.get('https://aa-t1dv-sfb.azurewebsites.net/api/selfassessment/' + id).then(function (response) {
-                        $scope.sad = response.data;                        
+                    $http.get('https://aa-t1dv-sfb.azurewebsites.net/api/selfassessment/' + $scope.id).then(function (response) {
+                        $scope.sad = response.data;
+                        $scope.teachingStaff = self.getTeachingStaff();
                     });
                 };
 
-                //self.totalSelectCount = function () {
-                //    var count = 0;
-                //    if ($scope.selectionList) {
-                //        count = _.reduce($scope.selectionList.HierarchicalCharts,
-                //            function (sum, group) {
-                //                return sum + self.groupSelectCount(group);
-                //            },
-                //            0);
-                //    }
-                //    return count;
-                //};
+                self.getTeachingStaff = function () {
+                    var ts = self.findAssessmentArea("Teaching staff");
+                    var percentage = ((ts.schoolDataLatestTerm / ts.totalForAreaTypeLatestTerm) * 100).toFixed(1);
+                    return {
+                        percentage: percentage,
+                        allBands: ts.allTresholds,
+                        matchedBand: self.findMatchingBand(ts.allTresholds, Number(percentage) / 100)
+                    };
+                }
 
                 self.phase = function () {
                     if ($scope.sad.hasSixthForm && $scope.sad.overallPhase == 'Secondary') {
                         return $scope.sad.overallPhase + ' with sixth form';
-                        }
+                    }
                     return $scope.sad.overallPhase;
                 }
 
+                self.findAssessmentArea = function (name) {
+                    var selection = _.find($scope.sad.sadAssesmentAreas,
+                        function (aa) {
+                            return aa.assessmentAreaName === name;
+                        });
+
+                    return selection;
+                }
+
+                self.findMatchingBand = function (allBands, value) {
+                    var selection = _.find(allBands,
+                        function (band) {
+                            return (band.scoreLow <= value) && (band.scoreHigh >= value);
+                        });
+
+                    selection.isMatch = true;
+                    return selection;
+                }
+
+                //////
                 self.loadData();
             }
         ]);
