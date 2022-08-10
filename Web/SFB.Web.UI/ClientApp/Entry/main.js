ddb2d92a5724d51f337c92bd76ce485290cdfc00
Jon Banner <jon@jonbanner.net>
2022-08-10 16:12:14 +0100

    54129 cookie banner functional tweaks

--- Web/SFB.Web.UI/ClientApp/Entry/main.js
+++ Web/SFB.Web.UI/ClientApp/Entry/main.js
@@ -3,5 +3,10 @@
 import cookiePolicy from '../ApplicationModules/cookiePolicy';
 import ComparisonList from '../ApplicationModules/ComparisonList';*/
 
+import CookiePolicy from '../AppModules/cookiePolicy';
+
+CookiePolicy.managePreferencesUi();
+CookiePolicy.bannerActions();
+CookiePolicy.manageRecruitmentNotification();
 initAll();
 
